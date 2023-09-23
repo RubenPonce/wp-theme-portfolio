3cc63aeef058005216cdfe9577c1f1bf1352da8e
rubenponce <rubenmartinponce@gmail.com>
2023-09-23 15:19:33 -0400

    FEAT: Hero section with CTA

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php get_header(); ?>
 
+    <main class="hero-section" style="background:linear-gradient(rgba(0, 0, 0, .1), rgba(0, 0, 0, .1)), url(<?php echo get_theme_file_uri('/images/tech-hero.webp') ?>);">
+        <div class="hero-section__content">
+            <h1 class="hero-section__headline">Crafting WordPress & React Solutions</h1>
+            <p class="hero-section__subheadline">Building Bespoken Websites & JavaScript Applications</p>
+            <p class="hero-section__description">Experienced web developer specializing in WordPress, React, and general JavaScript applications. Transforming your ideas into digital realities.</p>
+            <a href="https://calendly.com/rubenponce/have-a-conversation" class="hero-section__cta">Schedule a Consultation</a>
+        </div>
+    </main>
+
     <h2 id="workExperienceToggle" class="work-experience__title">Work Experience</h2>
     <div id="workExperienceSidebar" class="work-experience__sidebar">
         <span id="workExperienceClose" class="work-experience__close">X</span>
