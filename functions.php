6a2258f76b5eea5cba403852b536134ca0972e69
rubenponce <rubenmartinponce@gmail.com>
2023-09-24 10:54:18 -0400

    [Over Designing]: Social Media Icons & Font Awesome reformatted code

--- functions.php
+++ functions.php
@@ -3,6 +3,8 @@ function myportfolio_files(){
     wp_enqueue_style('myportfolio_main_styles', get_theme_file_uri("/css/index-style.css"));
     wp_enqueue_script( 'myportfolio-main-js', get_template_directory_uri() . '/javascript/main.js', array(), '1.0', true );
     wp_enqueue_style( 'myportfolio-style', get_stylesheet_uri() );
+    wp_enqueue_script('Font Awesome', 'https://kit.fontawesome.com/4810a47f7e.js', array(), '1.0', true);
+
 }
 add_action('wp_enqueue_scripts', 'myportfolio_files');
 
--- header.php
+++ header.php
@@ -1,22 +1,29 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-  <head>
+<head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
-  </head>
-  <body <?php body_class(); ?>>
-  <header class="site-header">
+</head>
+<body <?php body_class(); ?>>
+<header class="site-header">
     <div class="nav-bar-container">
-      <h1 class="logo"><a href="<?php echo site_url() ?>"><strong>Ann Arbor </strong>Web Design</a></h1>
-      <nav class="main-navigation">
-          <ul>
-            <li <?php if (is_front_page() ) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/') ?>">Home</a></li>
-            <li <?php if (is_page('about-me') or wp_get_post_parent_id(0) == 16) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-me') ?>">About Me</a></li>
-            <li><a href="#">Projects</a></li>
-            <li><a href="#">Contact</a></li>
-          </ul>
-        </nav>
+        <h1 class="logo"><a href="<?php echo site_url() ?>"><strong>Ann Arbor </strong>Web Design</a></h1>
+        <nav class="main-navigation">
+            <ul>
+                <li <?php if (is_front_page()) echo 'class="current-menu-item"' ?>><a
+                            href="<?php echo site_url('/') ?>">Home</a></li>
+                <li <?php if (is_page('about-me') or wp_get_post_parent_id(0) == 16) echo 'class="current-menu-item"' ?>>
+                    <a href="<?php echo site_url('/about-me') ?>">About Me</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Contact</a></li>
+                <div class="vertical-line"></div>
+                <li><a href="https://www.linkedin.com/in/rubenponce/" target="_blank"><i
+                                class="fab fa-linkedin social"></i></a></li>
+                <li><a href="https://x.com/krebdev" target="_blank"><i class="fab fa-twitter social"></i></a></li>
+                <li><a href="https://github.com/rubenponce" target="_blank"><i class="fab fa-github social"></i></a>
+                </li>
+            </ul>
     </div>
-    </header>
+</header>
 
